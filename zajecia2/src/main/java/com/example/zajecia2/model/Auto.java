8c9a11f5cd3424115c214b28fbded738a7d9fae6
KrzysztofBabs <153236574+KrzysztofBabs@users.noreply.github.com>
2024-10-16 20:31:06 +0200

    projekt3

--- zajecia2/src/main/java/com/example/zajecia2/model/Auto.java
+++ zajecia2/src/main/java/com/example/zajecia2/model/Auto.java
@@ -1,13 +1,29 @@
 package com.example.zajecia2.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Auto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String model;
     private int rokProdukcji;
+    private int identyfikator;
+
+
 
     public Auto(String model, int rokProdukcji) {
         this.model = model;
         this.rokProdukcji = rokProdukcji;
+        this.setIdentyfikator();
+
     }
+    public Auto(){};
+
 
     public String getModel() {
         return model;
@@ -23,5 +39,32 @@ public class Auto {
 
     public void setRokProdukcji(int rokProdukcji) {
         this.rokProdukcji = rokProdukcji;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getIdentyfikator() {
+        return identyfikator;
+    }
+
+    public void setIdentyfikator() {
+        char[] tablica1 = this.model.toCharArray();
+        char[] tablica2 = ("" + this.rokProdukcji).toCharArray();
+        int a=0;
+        for(char x : tablica1){
+            a+=(int) x;
+        }
+        for(char x : tablica2){
+            a+=(int) x;
+        }
+        this.identyfikator = a;
+
+
     }
 }
--- zajecia2/src/main/java/com/example/zajecia2/services/AutoService.java
+++ zajecia2/src/main/java/com/example/zajecia2/services/AutoService.java
@@ -2,49 +2,79 @@ package com.example.zajecia2.services;
 
 import com.example.zajecia2.controllers.MyRestController;
 import com.example.zajecia2.model.Auto;
+import com.example.zajecia2.repository.AutoRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
-@Component
+import static org.antlr.v4.runtime.tree.xpath.XPath.findAll;
 
-public class AutoService {
-    List<Auto> listaAut = new ArrayList<>();
-    public AutoService(){
-        listaAut.add(new Auto("Dacia", 2022));
-        listaAut.add(new Auto("Audi", 2023));
-        listaAut.add(new Auto("Toyota", 2024));
+//@Component
+@Service
+public class AutoService{
+    private AutoRepository repository;
 
+    public AutoService(AutoRepository repository){
+        this.repository = repository;
+        repository.save(new Auto("Dacia", 2022));
+        repository.save(new Auto("Audi", 2023));
+        repository.save(new Auto("Toyota", 2024));
     }
+
+    public Optional<Auto> getById(Long id){
+        return this.repository.findById(id);
+    }
+
+    public List<Auto> getByModel(String model){
+        return this.repository.findByModel(model);
+    }
+
+    public List<Auto> getByRokProdukcji(int rokProdukcji){
+        return this.repository.findByRokProdukcji(rokProdukcji);
+    }
+
+
     public List<Auto> getAll(){
-        return this.listaAut;
-    }
-    public void add(Auto auto){
-        this.listaAut.add(auto);
+        return this.repository.findAll();
     }
 
     public void delete(String model){
-        listaAut.removeIf(auto -> auto.getModel().equalsIgnoreCase(model));
-
-    }
-    public List<Auto> poRokuProdukcji(int rokProdukcji){
-        return listaAut.stream().filter(auto -> auto.getRokProdukcji()==rokProdukcji).collect(Collectors.toList());
-
+        List<Auto> auto = this.repository.findByModel(model);
+        this.repository.deleteAll(auto);
     }
 
-    public void AktualizujAuto(String model,Auto Noweauto){
-        for(int i=0;i<listaAut.size();i++){
-            Auto auto = listaAut.get(i);
-            if(auto.getModel().equalsIgnoreCase(model)){
-                auto.setModel(Noweauto.getModel());
-                auto.setRokProdukcji(Noweauto.getRokProdukcji());
+    public void add(Auto auto){
+        this.repository.save(new Auto(auto.getModel(), auto.getRokProdukcji()));
 
-            }
+    }
+
+    public void update(Auto auto){
+        Long id = auto.getId();
+        String model = auto.getModel();
+        int rokProdukcji = auto.getRokProdukcji();
+        Optional<Auto> car = this.repository.findById(id);
+        if(car.isPresent()){
+            Auto noweAuto = car.get();
+            noweAuto.setModel(model);
+            noweAuto.setRokProdukcji(rokProdukcji);
+            noweAuto.setIdentyfikator();
+            this.repository.save(noweAuto);
+        }
+        else{
+            System.out.println("nie podano dobrego id!");
         }
 
     }
 
 
+
+
 }
+
+
+
+
